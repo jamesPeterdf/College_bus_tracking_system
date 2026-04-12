e5c0f0b4fdbbec2ee7c0730204f114297e65430d
0xgokkull <gokkull04@gmail.com>
2026-04-13 01:33:58 +0530

    cleanup: remove legacy backend/generate-hash.js

--- backend/generate-hash.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require('bcrypt');
-async function run() {
-    const hash = await bcrypt.hash('admin123', 10);
-    console.log("New hash for admin123:");
-    console.log(hash);
-}
-run();
